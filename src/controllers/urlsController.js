27e31d877c9062c47ed1f6e1d605657694058e2b
Enzo <enzo202p@gmail.com>
2022-10-13 14:45:52 -0300

    feat: rotas de url delete e url open

--- src/controllers/urlsController.js
+++ src/controllers/urlsController.js
@@ -10,7 +10,12 @@ const shortenUrl = async (req, res) => {
         const { url } = req.body;
 
         const urlSchema = joi.object({
-            url: joi.string().uri().required()
+            url: joi.string().uri({
+                scheme: [
+                  'https',
+                  'http'
+                ]
+              }).required()
         });
 
         const validation = urlSchema.validate({ ...req.body }, { abortEarly: false });
@@ -20,7 +25,6 @@ const shortenUrl = async (req, res) => {
         }
 
         const shortenUrl = nanoid(8);
-        console.log(res.locals.userId);
 
         await connection.query('INSERT INTO urls (url, short_url, user_id) VALUES ($1, $2, $3)', [url, shortenUrl, res.locals.userId]);
 
@@ -42,7 +46,6 @@ const getUrlId = async (req, res) => {
             return res.sendStatus(404);
         }
 
-        console.log(url.rows[0]);
         delete url.rows[0].user_id;
         delete url.rows[0].createdAt;
 
@@ -53,4 +56,46 @@ const getUrlId = async (req, res) => {
     }
 }
 
-export { shortenUrl, getUrlId }
+const openUrl = async (req,res) => {
+
+    try{
+        const { shortUrl } = req.params;
+        const url = await connection.query('SELECT * FROM urls WHERE short_url=$1', [shortUrl]);
+
+        if(!url.rows[0]){
+            return res.sendStatus(404);
+        }
+        console.log(url.rows);
+        await connection.query('INSERT INTO url_visits (url_id) VALUES ($1)', [url.rows[0].id]);
+
+        res.redirect(url.rows[0].url)
+    } catch ( err ){
+        console.log(err);
+        res.sendStatus(500);
+    }
+}
+
+const deleteUrl = async (req,res) => {
+    try{
+        const { id } = req.params;
+
+        const url = await connection.query('SELECT * FROM urls WHERE id=$1', [id]);
+
+        if(!url.rows[0]){
+            return res.sendStatus(404);
+        }
+
+        if(url.rows[0].user_id !== res.locals.userId){
+            return res.sendStatus(401);
+        }
+
+        await connection.query('DELETE FROM urls WHERE id=$1', [id]);
+
+        res.sendStatus(204);
+    }catch (err){
+        console.log(err);
+        res.sendStatus(500);
+    }
+}
+
+export { shortenUrl, getUrlId, openUrl, deleteUrl }
--- src/routers/urlsRouter.js
+++ src/routers/urlsRouter.js
@@ -1,10 +1,12 @@
 import express from 'express';
-import { shortenUrl, getUrlId } from '../controllers/urlsController.js';
+import { shortenUrl, getUrlId, openUrl, deleteUrl } from '../controllers/urlsController.js';
 import { verifyToken } from '../middlewares/tokenMiddleware.js';
 
 const urlsRouter = express.Router();
 
 urlsRouter.post("/urls/shorten", verifyToken, shortenUrl);
 urlsRouter.get("/urls/:id", getUrlId);
+urlsRouter.get("/urls/open/:shortUrl", openUrl);
+urlsRouter.delete("/urls/:id", verifyToken, deleteUrl);
 
 export { urlsRouter }
